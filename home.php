0e79c40a7cdecd99f2f17cdf0610d38fb782ff09
smilehee18 <123307856+smilehee18@users.noreply.github.com>
2023-02-06 19:36:55 +0900

    Update home.php

--- home.php
+++ home.php
@@ -12,49 +12,10 @@ if(isset($_SESSION['login']))
 ?>
 <html>
 <head>
-	<style>
-	html
-	{
-		background-color : #FFE4B5;
-	    background-image: url('https://cdn.pixabay.com/photo/2017/01/31/00/09/books-2022464_960_720.png');
-	    background-repeat : no-repeat;
-	    background-position: right top;
-	}
-
-	body
-	{
-	  background-image : url('https://cdn.pixabay.com/photo/2016/01/11/16/06/bookmark-1133851_960_720.png');
-     background-repeat : no-repeat;
-     background-position: top;
-	  width : 700PX;
-	  height : 1500px;
-	  margin : 1em auto;
-      background-color: #FDF5E6;
-	  border : 10px #CCFF33 double;
-	  text-align: center;
-
-	}
-	.btn1
-	{
-     color: steelblue;
-     display: block;
-	 background-color:white;
-	 padding: 30px 30px;
-	 text-align: center;
-	 font-size: 40px;
-	 text-decoration: none;
-	 border: 3px outset lime;
-	 border-radius :10px;
-	}
-	.btn1:hover
-	{
-	  background-color: limegreen;
-	  color: greenyellow;
-	}
-	</style>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>home.php</title>
+	<link href='style/style.css' rel='stylesheet'></link>
 </head>
 <body>
 	<h2> Welcome to Free Book-Sharing Homepage !
