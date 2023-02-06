ca27dc32539483517eafdf423b9309a61cb733e3
smilehee18 <123307856+smilehee18@users.noreply.github.com>
2023-02-06 19:30:26 +0900

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -27,44 +27,7 @@ if(isset($_POST['submit']))
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Admin_Login</title>
-	<style>
-	html 
-	{
-	  background-color : #FFE4B5;
-	  background-image: url('https://cdn.pixabay.com/photo/2017/01/31/00/09/books-2022464_960_720.png');
-	  background-repeat : no-repeat;
-	  background-position: right top;
-	}
-	body
-	{
-	  width : 700PX;
-	  height : 1500px;
-	  margin : 1em auto;
-      background-color: #FDF5E6;
-	  border : 10px #CCFF33 double;
-	  text-align: center;
-	}
-	a
-	{
-      color: blue;
-	}
-	.red
-	{
-	  color: red;
-	}
-	table
-	{
-		border-collapse: collapse;
-	}
-	th, td {
-    padding: 15px;
-    text-align: center;
-  }
-  th 
-  {
-  	background-color: greenyellow;
-  }
-	</style>
+	<link href='style/style.css' rel='stylesheet'></link>
 </head>
 <body>
 	<h2> Welcome to Free Book - Sharing HomePage ! </h2>
